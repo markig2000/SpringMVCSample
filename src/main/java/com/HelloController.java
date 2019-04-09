7bce21a9cffc4122542c1b3b878d31b260a88670
markig2000 <42933694+markig2000@users.noreply.github.com>
2019-04-09 14:44:15 +0530

    Update HelloController.java

--- src/main/java/com/HelloController.java
+++ src/main/java/com/HelloController.java
@@ -1,1 +1,30 @@
 
+package com;
+
+import java.io.FileInputStream;
+import java.util.Base64;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+
+@Controller
+public class HelloController {
+
+	/*@RequestMapping("/hello")
+	public ModelAndView mymethod(){
+		return new ModelAndView("hellopage","msg","Hello First Spring");
+	}*/
+	
+	@RequestMapping("/hello")
+	public ModelAndView mymethod(){
+		
+		
+		
+		return new ModelAndView("hellopage","msg","Hello First Spring");
+	}
+}

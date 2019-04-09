4b5b7d4f081d67c688db0eaf3b90a78e9b98bb0e
markig2000 <42933694+markig2000@users.noreply.github.com>
2019-04-09 14:44:38 +0530

    Update WelcomeController.java

--- src/main/java/com/WelcomeController.java
+++ src/main/java/com/WelcomeController.java
@@ -1,1 +1,15 @@
 
+package com;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class WelcomeController {
+
+	@RequestMapping("/welcome")
+	public ModelAndView mymethod(){
+		return new ModelAndView("welcomepage");
+	}
+}
